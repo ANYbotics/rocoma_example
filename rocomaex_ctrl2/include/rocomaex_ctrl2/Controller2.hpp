bc433e8ab20884d9ca66467010f3d7a7c3e99d7b
Gabriel Hottiger <hogabrie@student.ethz.ch>
2016-07-29 16:52:36 +0200

    updated second example controller

--- rocomaex_ctrl2/include/rocomaex_ctrl2/Controller2.hpp
+++ rocomaex_ctrl2/include/rocomaex_ctrl2/Controller2.hpp
@@ -1,36 +1,78 @@
-/*
- * Crawling_Task.hpp
+/**
+ * Copyright (c) 2016, Gabriel Hottiger
+ * All rights reserved.
  *
- *  Created on: Aug 20, 2024
- *      Author: C. Dario Bellicoso
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Robotics Systems Lab, ETH Zurich nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL  Gabriel Hottiger BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+/*!
+ * @file    Controller2.hpp
+ * @author  Gabriel Hottiger
+ * @date    Jul, 2016
  */
 
 #pragma once
 
+// roco
+#include "roco/controllers/Controller.hpp"
+
+// rocomaex model
 #include "rocomaex_model/State.hpp"
 #include "rocomaex_model/Command.hpp"
 
-#include "roco/controllers/Controller.hpp"
 
 namespace rocomaex_ctrl2 {
 
-class Controller2: public roco::Controller<rocomaex_model::State, rocomaex_model::Command>
-  {
-  public:
-    typedef roco::Controller<rocomaex_model::State, rocomaex_model::Command> Base;
+//! Example class for a robot controller implementing the roco::ControllerAdapteeInterface
+/**
+ * IMPORTANT: Make sure to use virtual inheritance, prevents issues due to multiple inheritance
+ */
+class Controller2: virtual public roco::Controller<rocomaex_model::State, rocomaex_model::Command>
+{
 
-  public:
+ public:
+  //! Convenience typedef for Base class
+  typedef roco::Controller<rocomaex_model::State, rocomaex_model::Command> Base;
 
-    Controller2();
-    virtual ~Controller2();
+ public:
+  /** Default Constructor
+      NOTE: Other constructors can't be invoked when using the plugin mechanism
+   */
+  Controller2();
+  //! Destructor
+  virtual ~Controller2();
 
-    virtual bool create(double dt);
-    virtual bool initialize(double dt);
-    virtual bool advance(double dt);
-    virtual bool reset(double dt);
-    virtual bool cleanup();
-    virtual bool stop();
-    virtual bool preStop();
+  /** Adaptee Implementation
+        NOTE: Adaptee functionality is hidden from the client
+   */
+ protected:
+  virtual bool create(double dt);
+  virtual bool initialize(double dt);
+  virtual bool advance(double dt);
+  virtual bool reset(double dt);
+  virtual bool cleanup();
+  virtual bool stop();
+  virtual bool preStop();
 
 };
 
--- rocomaex_ctrl2/src/Controller2.cpp
+++ rocomaex_ctrl2/src/Controller2.cpp
@@ -1,4 +1,54 @@
+/**
+ * Copyright (c) 2016, Gabriel Hottiger
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Robotics Systems Lab, ETH Zurich nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL  Gabriel Hottiger BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+/*!
+ * @file    Controller2.cpp
+ * @author  Gabriel Hottiger
+ * @date    Jul, 2016
+ */
+
+// rocomaex ctrl2
 #include "rocomaex_ctrl2/Controller2.hpp"
+
+// pluginlib
+#include <pluginlib/class_list_macros.h>
+
+// rocoma plugin
+#include "rocoma_plugin/interfaces/ControllerPluginInterface.hpp"
+#include "rocoma_plugin/plugins/ControllerPlugin.hpp"
+
+/**
+ * Helper macro that wraps pluginlib's PLUGINLIB_EXPORT_CLASS(). Needed because of complicated template syntax.
+ * @param Name of the controller plugin, also name of derived class in plugin xml file
+ * @param Type of the controller the plugin should be registered for (this controller)
+ * @param State class the controller is templated on
+ * @param Command class the controller is templated on
+ */
+ROCOMA_EXPORT_CONTROLLER(Controller2Plugin, rocomaex_ctrl2::Controller2, rocomaex_model::State, rocomaex_model::Command)
 
 namespace rocomaex_ctrl2 {
 
