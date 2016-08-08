2f56f997ca59ec2e81a8d41a5d1927ea19dd5755
Gabriel <hogabrie@student.ethz.ch>
2016-08-08 16:09:26 +0200

    Added emergency controller ros to example

--- rocoma_example/src/RocomaExample.cpp
+++ rocoma_example/src/RocomaExample.cpp
@@ -29,9 +29,9 @@ void RocomaExample::init()
   std::shared_ptr<any_worker::WorkerManager> workerManager(new any_worker::WorkerManager());
 
   using COptions = rocoma_ros::ControllerManagerRos<rocomaex_model::State, rocomaex_model::Command>::ControllerPairOptions;
-  std::vector< COptions > controllerNameMap = {  COptions{"Controller1Plugin", "EmergencyController1Plugin",false, false},
+  std::vector< COptions > controllerNameMap = {  COptions{"Controller1Plugin", "EmergencyControllerRos1Plugin",false, true},
                                                  COptions{"Controller2Plugin", "EmergencyController2Plugin", false, false},
-                                                 COptions{"ControllerRos1Plugin", "EmergencyController3Plugin", true, false} };
+                                                 COptions{"ControllerRos1Plugin", "EmergencyController1Plugin", true, false} };
   std::string failproofControllerName {"FailproofController1Plugin"};
 
   controllerManager_.setupControllers(failproofControllerName,
---
+++ rocomaex_ros_emgcy_ctrl1/include/rocomaex_ros_emgcy_ctrl1/EmergencyControllerRos1.hpp
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2016, Gabriel Hottiger
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Robotics Systems Lab, ETH Zurich nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL  Gabriel Hottiger BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+/*!
+ * @file    EmergencyControllerRos1.hpp
+ * @author  Gabriel Hottiger
+ * @date    Jul, 2016
+ */
+
+#pragma once
+
+// roco
+#include "roco/controllers/ControllerRos.hpp"
+#include "roco/controllers/adaptees/EmergencyControllerAdapteeInterface.hpp"
+
+// rocomaex model
+#include "rocomaex_model/State.hpp"
+#include "rocomaex_model/Command.hpp"
+
+
+namespace rocomaex_ros_emgcy_ctrl1 {
+
+//! Example class for a robot controller implementing the roco::ControllerAdapteeInterface
+/**
+ * IMPORTANT: Make sure to use virtual inheritance, prevents issues due to multiple inheritance
+ */
+class EmergencyControllerRos1: virtual public roco::ControllerRos<rocomaex_model::State, rocomaex_model::Command>, public roco::EmergencyControllerAdapteeInterface
+{
+
+ public:
+  //! Convenience typedef for Base class
+  typedef roco::ControllerRos<rocomaex_model::State, rocomaex_model::Command> Base;
+
+ public:
+  /** Default Constructor
+      NOTE: Other constructors can't be invoked when using the plugin mechanism
+   */
+  EmergencyControllerRos1();
+  //! Destructor
+  virtual ~EmergencyControllerRos1();
+
+  /** Adaptee Implementation
+        NOTE: Adaptee functionality is hidden from the client
+   */
+ protected:
+  virtual bool create(double dt);
+  virtual bool initialize(double dt);
+  virtual bool advance(double dt);
+  virtual bool reset(double dt);
+  virtual bool cleanup();
+  virtual bool stop();
+  virtual bool preStop();
+  virtual bool initializeFast(double dt);
+
+};
+
+} // namespace rocomaex_ros_emgcy_ctrl1
