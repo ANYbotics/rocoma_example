56ea84a819b12bbb3c5f44f4b34ec76ee281cca2
Gabriel Hottiger <hogabrie@student.ethz.ch>
2016-11-24 11:17:42 +0100

    rocoma_example ros independent, use to improve controller manager

--- rocoma_example/include/rocoma_example/RocomaExample.hpp
+++ rocoma_example/include/rocoma_example/RocomaExample.hpp
@@ -4,16 +4,8 @@
 #include "rocomaex_model/State.hpp"
 #include "rocomaex_model/Command.hpp"
 
-// rocomaex controllers
-#include "rocomaex_failproof_ctrl1/FailproofController1.hpp"
-#include "rocomaex_ctrl1/Controller1.hpp"
-#include "rocomaex_ctrl2/Controller2.hpp"
-#include "rocomaex_emgcy_ctrl1/EmergencyController1.hpp"
-#include "rocomaex_emgcy_ctrl2/EmergencyController2.hpp"
-
 // rocoma
 #include "rocoma/ControllerManager.hpp"
-#include "rocoma/controllers/adapters.hpp"
 
 // Boost
 #include <boost/thread.hpp>
@@ -25,26 +17,19 @@ namespace rocoma_example {
 
 class RocomaExample{
 public:
-  RocomaExample(const double timestep);
-  virtual ~RocomaExample();
+	RocomaExample(const double timestep);
+	virtual ~RocomaExample();
 
-  virtual void init();
-  virtual void cleanup();
-  virtual bool update();
+	virtual void init();
+	virtual void cleanup();
+	virtual bool update();
 
-protected:
-  using FpAdapter = rocoma::FailproofControllerAdapter< rocomaex_failproof_ctrl1::FailproofController1, rocomaex_model::State, rocomaex_model::Command >;
-  using Emgcy1Adapter = rocoma::EmergencyControllerAdapter< rocomaex_emgcy_ctrl1::EmergencyController1, rocomaex_model::State, rocomaex_model::Command >;
-  using Emgcy2Adapter = rocoma::EmergencyControllerAdapter< rocomaex_emgcy_ctrl2::EmergencyController2, rocomaex_model::State, rocomaex_model::Command >;
-  using Ctrl1Adapter = rocoma::ControllerAdapter< rocomaex_ctrl1::Controller1, rocomaex_model::State, rocomaex_model::Command >;
-  using Ctrl2Adapter = rocoma::ControllerAdapter< rocomaex_ctrl2::Controller2, rocomaex_model::State, rocomaex_model::Command >;
-
- private:
-  rocoma::ControllerManager controllerManager_;
-  std::shared_ptr<rocomaex_model::State> state_;
-  std::shared_ptr<rocomaex_model::Command> command_;
-  std::shared_ptr<boost::shared_mutex> mutexState_;
-  std::shared_ptr<boost::shared_mutex> mutexCommand_;
+private:
+	rocoma::ControllerManager controllerManager_;
+	std::shared_ptr<rocomaex_model::State> state_;
+	std::shared_ptr<rocomaex_model::Command> command_;
+	std::shared_ptr<boost::shared_mutex> mutexState_;
+	std::shared_ptr<boost::shared_mutex> mutexCommand_;
 };
 
 }
--- rocoma_example/src/RocomaExample.cpp
+++ rocoma_example/src/RocomaExample.cpp
@@ -1,12 +1,18 @@
 // rocoma example
 #include "rocoma_example/RocomaExample.hpp"
 
+// rocoma
+#include "rocoma/controllers/adapters.hpp"
+
 // rocomaex controllers
 #include "rocomaex_failproof_ctrl1/FailproofController1.hpp"
 #include "rocomaex_ctrl1/Controller1.hpp"
 #include "rocomaex_ctrl2/Controller2.hpp"
 #include "rocomaex_emgcy_ctrl1/EmergencyController1.hpp"
 #include "rocomaex_emgcy_ctrl2/EmergencyController2.hpp"
+
+// roco
+#include "roco/controllers/controllers.hpp"
 
 // message logger
 #include "message_logger/message_logger.hpp"
@@ -28,47 +34,101 @@ RocomaExample::~RocomaExample()
 
 void RocomaExample::init()
 {
-  // convinience
-  using FailproofControllerPtr = rocoma::ControllerManager::FailproofControllerPtr;
-  using EmgcyControllerPtr = rocoma::ControllerManager::EmgcyControllerPtr;
-  using ControllerPtr = rocoma::ControllerManager::ControllerPtr;
-
-  // Setup failproof controller
-  FpAdapter* failproof = new FpAdapter();
-  failproof->setName("Shutoff");
-  failproof->setStateAndCommand(state_, mutexState_, command_, mutexCommand_);
-  failproof->setParameterPath("parameter.xml");
-
-  // Setup first emergency controller
-  Emgcy1Adapter* emgcy_ctrl1 = new Emgcy1Adapter();
-  emgcy_ctrl1->setName("Recover");
-  emgcy_ctrl1->setStateAndCommand(state_, mutexState_, command_, mutexCommand_);
-  emgcy_ctrl1->setParameterPath("emgcy_parameters.xml");
-
-  // Setup first controller
-  Ctrl1Adapter* ctrl1 = new Ctrl1Adapter();
-  ctrl1->setName("DoFancyStuff");
-  ctrl1->setStateAndCommand(state_, mutexState_, command_, mutexCommand_);
-  ctrl1->setParameterPath("emgcy_parameters.xml");
-
-  // Setup second controller
-  Ctrl2Adapter* ctrl2 = new Ctrl2Adapter();
-  ctrl2->setName("DoStuff");
-  ctrl2->setStateAndCommand(state_, mutexState_, command_, mutexCommand_);
-  ctrl2->setParameterPath("emgcy_parameters.xml");
+  /*** |--- EXAMPLE DESCRIPTION
+   *   Let's assume a quadruped with an additional arm for manipulations. The quadruped can walk and grasp things,
+   *   which are located on top of its torso. When walking fails or an emergency stop occurs it moves into a standing
+   *   position. If the grasping task fails it moves the arm to a default configuration. It can also execute the tasks
+   *   simultaneously, but does not know a proper reaction to a failure and thus freezes all the joints. There is also
+   *   a mode for standing and grasping at the same time, to recover from failure the arm is set to default.
+       ---|
+   */
 
 
-  // Make pairs
-  controllerManager_.setFailproofController(FailproofControllerPtr(failproof));
-  controllerManager_.addControllerPair(ControllerPtr(ctrl1), EmgcyControllerPtr(emgcy_ctrl1));
-  controllerManager_.addControllerPairWithExistingEmergencyController(ControllerPtr(ctrl2), "Recover");
+  /*** The controller adapter type names are quite long. For convenience one can define typedefs that
+   *   correspond to the used controller adapters. Note the last controller, which is a tuple type.
+   *   This type allows to pair an arbitrary number of controllers to a single controller tuple, in which
+   *   the controllers are executed in series (same order as in type specification).
+   */
+  using FreezeCtrl = rocoma::FailproofControllerAdapter< rocomaex_failproof_ctrl1::FailproofController1, rocomaex_model::State, rocomaex_model::Command >;
+  using StandCtrl = rocoma::EmergencyControllerAdapter< rocomaex_emgcy_ctrl1::EmergencyController1, rocomaex_model::State, rocomaex_model::Command >;
+  using ArmDefaultCtrl = rocoma::EmergencyControllerAdapter< rocomaex_emgcy_ctrl2::EmergencyController2, rocomaex_model::State, rocomaex_model::Command >;
+  using WalkCtrl = rocoma::ControllerAdapter< rocomaex_ctrl1::Controller1, rocomaex_model::State, rocomaex_model::Command >;
+  using GraspCtrl = rocoma::ControllerAdapter< rocomaex_ctrl2::Controller2, rocomaex_model::State, rocomaex_model::Command >;
+  using GraspAndWalkTuple = roco::ControllerTuple<rocomaex_model::State, rocomaex_model::Command, rocomaex_ctrl1::Controller1, rocomaex_ctrl2::Controller2>;
+  using GraspAndWalkCtrl = rocoma::ControllerAdapter< GraspAndWalkTuple, rocomaex_model::State, rocomaex_model::Command >;
+  using GraspAndStandTuple = roco::ControllerTuple<rocomaex_model::State, rocomaex_model::Command, rocomaex_emgcy_ctrl1::EmergencyController1, rocomaex_ctrl2::Controller2>;
+  using GraspAndStandCtrl = rocoma::ControllerAdapter< GraspAndStandTuple, rocomaex_model::State, rocomaex_model::Command >;
 
-  // These controllers are now owned by the controller manager
-//  controllerManager_.addControllerPair(std::move(controller2), std::move(emergency3));
+  /*** Controllers usually have a default name, use setName to override the default name. The same holds for the parameter
+   *   path. The path is an absolute path or relative path to the current directory at execution time.
+   *   Since the controller manager only knows the controller by it's interface and we are using the roco::Controller class
+   *   in all of our controllers, we have to specify state and command and their corresponding mutexes before adding the
+   *   controller to the manager.
+   */
 
-  // Initialize Controller Manager
+  /*** Setup a fail-proof controller that freezes all joints. This controller is absolutely necessary
+   *   for the controller manager to be executed. We use a unique_ptr to indicate the ownership of the controller.
+   */
+  std::unique_ptr<FreezeCtrl> freeze(new FreezeCtrl());
+  freeze->setName("Freeze");
+  freeze->setParameterPath("");
+  freeze->setStateAndCommand(state_, mutexState_, command_, mutexCommand_);
+
+  //--- Setup all emergency controllers. Again we use a unique_ptr to indicate the ownership of the controller.
+
+  //! Setup a controller that brings the quadruped into a standing position.
+  std::unique_ptr<StandCtrl> stand(new StandCtrl());
+  stand->setName("Stand");
+  stand->setStateAndCommand(state_, mutexState_, command_, mutexCommand_);
+  stand->setParameterPath("stand.yaml");
+
+  //! Setup a controller that resets the arm to a default configuration.
+  std::unique_ptr<ArmDefaultCtrl> arm2Default(new ArmDefaultCtrl());
+  arm2Default->setName("ArmDefault");
+  arm2Default->setStateAndCommand(state_, mutexState_, command_, mutexCommand_);
+  arm2Default->setParameterPath("/home/user/params/armdefault.txt");
+
+  // ---
+
+  //--- Setup all controllers. Again we use a unique_ptr to indicate the ownership of the controller.
+
+  //! Setup controller that enables quadruped to walk.
+  std::unique_ptr<WalkCtrl> walk(new WalkCtrl());
+  walk->setName("Walk");
+  walk->setStateAndCommand(state_, mutexState_, command_, mutexCommand_);
+  walk->setParameterPath("param/walk/parameters.xml");
+
+  //! Setup controller that grasps things located on the quadruped's torso.
+  std::unique_ptr<GraspCtrl> grasp(new GraspCtrl());
+  grasp->setName("Grasp");
+  grasp->setStateAndCommand(state_, mutexState_, command_, mutexCommand_);
+  grasp->setParameterPath("param/grasp.txt");
+
+  //! Setup controller that combines the grasping and walking task.
+  std::unique_ptr<GraspAndWalkCtrl> graspAndWalk(new GraspAndWalkCtrl());
+  graspAndWalk->setName("WalkAndGrasp");
+  graspAndWalk->setStateAndCommand(state_, mutexState_, command_, mutexCommand_);
+  graspAndWalk->setParameterPath("walkAndGrasp.xml");
+
+  //! Setup controller that combines the grasping and standing task.
+  std::unique_ptr<GraspAndStandCtrl> graspAndStand(new GraspAndStandCtrl());
+  graspAndStand->setName("StandAndGrasp");
+  graspAndStand->setStateAndCommand(state_, mutexState_, command_, mutexCommand_);
+  graspAndStand->setParameterPath("standAndGrasp.yaml");
+
+  // ---
+
+  //! Initialize Controller Manager
   controllerManager_.setIsRealRobot(false);
   controllerManager_.setTimestep(1);
+
+  // Make pairs
+  controllerManager_.setFailproofController(std::move(freeze));
+  controllerManager_.addControllerPair(std::move(walk), std::move(stand));
+  controllerManager_.addControllerPair(std::move(grasp), std::move(arm2Default));
+  controllerManager_.addControllerPair(std::move(graspAndWalk), nullptr);
+  controllerManager_.addControllerPairWithExistingEmergencyController(std::move(graspAndStand), "ArmDefault");
+
 }
 
 void RocomaExample::cleanup()
